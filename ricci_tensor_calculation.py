1d72369a7d29a966e833f258cfb8fb80333a3c73
andreaprogram <andreaprogram@gmail.com>
2026-05-30 13:48:22 +0200

    implementation of latex to sp function so that the inputs are on tex format

--- ricci_tensor_calculation.py
+++ ricci_tensor_calculation.py
@@ -1,5 +1,15 @@
 import sympy as sp
 from sympy import pprint, symbols, diff, Matrix
+from sympy.parsing.latex import parse_latex 
+# parse-latex takes a string written in LaTeX-style math and converts it into a SymPy expression
+
+
+def texfy(expr): #let's define a function that takes a LaTeX string and returns a SymPy expression
+    try:
+        return parse_latex(expr)
+    except Exception as exc:
+        raise ValueError("Please enter a valid LaTeX expression.") from exc
+    
 
 # DIMENSION OF THE SPACETIME
 while True:
@@ -12,14 +22,14 @@ while True:
             print("d must be a positive integer greter than 0")
     
     except ValueError:
-        print(" Please, enter an integer")
+        print("Please, enter an integer")
 
 # COORDINATES
 x_mu = sp.Matrix.zeros(d,1)
 
 print("Now, let's define your coordinates. Please enter them one by one in LaTeX format (e.g., t, r, \\theta, \\phi).")
 for i in range(d):
-    x_mu[i] = sp.symbols(input(f'x**{i} = '))
+    x_mu[i] = texfy((input(f'x**{i} = ')))
 #print(x_mu)
 
 # METRIC TENSOR 
@@ -35,7 +45,7 @@ while True:
         nu = int(input("nu = "))
 
         if 0 <= mu < d and 0 <= nu < d:
-            g[mu, nu] = sp.sympify(input(f"g[{mu},{nu}] = "))
+            g[mu, nu] = texfy(input(f"g[{mu},{nu}] = "))
             g[nu, mu] = g[mu, nu]  # Imposing symmetry of the metric tensor
         else:
           print("mu and nu must be integers between 0 and", d)
